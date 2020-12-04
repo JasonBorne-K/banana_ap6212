383dd9f49ee45d38d5ef21d85ef7275da8ed00a8
JasonBorne-K <75062436+JasonBorne-K@users.noreply.github.com>
2020-12-04 16:13:04 +0900

    Add files via upload

---
+++ bash_sleep1_banana.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+
+mv 3MB.mdt A_3MB_1.mdt
+
+cnt=1
+old=1
+while (("${cnt}"<10001));do
+do
+	echo A_3MB_"${old}".mdt
+	curl -T A_3MB_"${old}".mdt -a -u Banana:seba12345! ftp://ebrave.iptime.org:12121 -m 60
+	if [ $? -ne 0 ]; then
+	break;
+	fi
+
+	((cnt="${cnt}"+1))
+	mv A_3MB_"${old}".mdt A_3MB_"${cnt}".mdt
+	((old="${old}"+1))
+	sleep 1
+done
+
+
+
+
+mv A_3MB_$old.mdt 3MB.mdt
+echo "TEST Finished!!!"
---
+++ sh_sleep1_Banana.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+
+
+mv 3MB.mdt A_3MB_1.mdt
+
+cnt=1
+old=1
+
+while true
+do
+
+echo A_3MB_"$old".mdt
+curl -T ./A_3MB_"$old".mdt -a -u Banana:seba12345! ftp://ebrave.iptime.org:12121 -m 60 
+if [ $? -ne 0 ]; then
+break;
+fi
+
+cnt=$((cnt+1))
+
+mv A_3MB_"$old".mdt A_3MB_"$cnt".mdt
+old=$((old+1))
+sleep 1
+if [ $old -eq 10001 ]; then
+break;
+fi
+
+done
+
+mv A_3MB_"${old}".mdt 3MB.mdt
+
+echo "Finished!!!"
